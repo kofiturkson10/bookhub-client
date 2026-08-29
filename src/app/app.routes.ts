4afa3340c133a9542a95f989e1e922c3f9713b85
Kofi <kofi.turkson10@gmail.com>
2026-08-29 16:48:19 +0200

    Angular 20 scaffold: Bootstrap, Font Awesome, routes for books/quotes/login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { Books } from './pages/books/books';
+import { Quotes } from './pages/quotes/quotes';
+import { Login } from './pages/login/login';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'books', pathMatch: 'full' },
+  { path: 'books', component: Books },
+  { path: 'quotes', component: Quotes },
+  { path: 'login', component: Login },
+  { path: '**', redirectTo: 'books' },
+];
